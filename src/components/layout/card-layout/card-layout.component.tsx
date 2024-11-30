3befc7d192deacd0db0baa2c4b4602500442df89
cout970 <thecout970@gmail.com>
2024-11-30 19:25:23 +0100

    Fix #4
    Allow for wider or larger content
    Add bottom section as optional

--- src/components/layout/card-layout/card-layout.component.tsx
+++ src/components/layout/card-layout/card-layout.component.tsx
@@ -5,16 +5,16 @@ import styles from "./card-layout.module.scss";
 import { BackgroundComponent, CardComponent } from "../../../components";
 // import { cn } from "src/utils";
 
-type Props = {} & React.HTMLProps<HTMLDivElement>;
+type Props = { actions?: React.ReactNode } & React.HTMLProps<HTMLDivElement>;
 
-export const CardLayoutComponent: React.FC<Props> = ({ children }) => {
+export const CardLayoutComponent: React.FC<Props> = ({ children, actions }) => {
   return (
     <BackgroundComponent>
       <div className={styles.wrapper}>
         <CardComponent className={styles.card}>
           <main className={styles.main}>
             <div className={styles.content}>{children}</div>
-            <div className={styles.actions}>aha</div>
+            {actions ? <div className={styles.actions}>{actions}</div> : null}
           </main>
         </CardComponent>
       </div>
--- src/components/layout/card-layout/card-layout.stories.tsx
+++ src/components/layout/card-layout/card-layout.stories.tsx
@@ -8,5 +8,75 @@ export default {
 export const Primary = {
   args: {
     children: <div>test</div>,
+    actions: "aha",
+  },
+};
+
+export const Long = {
+  args: {
+    children: (
+      <div>
+        <div>Line 1</div>
+        <div>Line 2</div>
+        <div>Line 3</div>
+        <div>Line 4</div>
+        <div>Line 5</div>
+        <div>Line 6</div>
+        <div>Line 7</div>
+        <div>Line 8</div>
+        <div>Line 9</div>
+        <div>Line 10</div>
+        <div>Line 11</div>
+        <div>Line 12</div>
+        <div>Line 13</div>
+        <div>Line 14</div>
+        <div>Line 15</div>
+        <div>Line 16</div>
+        <div>Line 17</div>
+        <div>Line 18</div>
+        <div>Line 19</div>
+        <div>Line 20</div>
+        <div>Line 21</div>
+        <div>Line 22</div>
+        <div>Line 23</div>
+        <div>Line 24</div>
+      </div>
+    ),
+    actions: "Help",
+  },
+};
+
+export const Wide = {
+  args: {
+    children: (
+      <div style={{ whiteSpace: "nowrap" }}>
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      </div>
+    ),
+    actions: "Help",
+  },
+};
+
+export const NoActions = {
+  args: {
+    children: (
+      <div>
+        <h1>Login</h1>
+      </div>
+    ),
+    actions: undefined,
+  },
+};
+
+export const Centering = {
+  args: {
+    children: (
+      <>
+        <div style={{ marginBottom: "auto" }}>Top</div>
+        <div>Center</div>
+        <div style={{ marginTop: "auto" }}>Bottom</div>
+      </>
+    ),
+    actions: "See more...",
   },
 };
