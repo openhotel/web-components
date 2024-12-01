550f57ff5b27c9573a8d93c31b94ca39508c55ee
cout970 <thecout970@gmail.com>
2024-12-01 16:58:43 +0100

    Run prettier on input component

--- src/components/form/input/input.component.tsx
+++ src/components/form/input/input.component.tsx
@@ -3,23 +3,35 @@ import * as React from "react";
 // @ts-ignore
 import styles from "./input.module.scss";
 import { cn } from "../../../utils";
-import {useId, useState} from "react";
+import { useId, useState } from "react";
 
 type Props = {
   className?: string;
   placeholder?: string;
 } & React.HTMLProps<HTMLInputElement>;
 
-export const InputComponent: React.FC<Props> = ({ className, placeholder, ...props }) => {
+export const InputComponent: React.FC<Props> = ({
+  className,
+  placeholder,
+  ...props
+}) => {
   const id = useId();
 
-  return <div className={styles.inputWrapper}>
-    <input
-      id={id}
-      className={cn(styles.input, className, {[styles.hasPlaceholder]: !!placeholder})}
-      placeholder={placeholder}
-      {...props}
-    />
-    {placeholder ? <label className={styles.placeholder} htmlFor={id}>{placeholder}</label> : null}
-  </div>;
+  return (
+    <div className={styles.inputWrapper}>
+      <input
+        id={id}
+        className={cn(styles.input, className, {
+          [styles.hasPlaceholder]: !!placeholder,
+        })}
+        placeholder={placeholder}
+        {...props}
+      />
+      {placeholder ? (
+        <label className={styles.placeholder} htmlFor={id}>
+          {placeholder}
+        </label>
+      ) : null}
+    </div>
+  );
 };
