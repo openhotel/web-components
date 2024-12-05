75ca952fb8aee43bac69afcbc74d50d8701e3306
Pablo González Rubio <pagoru@gmail.com>
2024-12-05 15:00:11 +0100

    fix: add scss exports on vite - fix #8 (#9)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import { build } from "./vite/plugins";
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
     dts({
       insertTypesEntry: true, // Generates a types entry file
     }),
+    build(),
   ],
   build: {
     lib: {
