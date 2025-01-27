3f147fcb9076734ce4d5d6108a24999e5e671afe
pagoru <pagoru@gmail.com>
2025-01-27 19:14:16 +0100

    fix: export utils and hooks - fix #71

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from "./components";
 export * from "./consts";
 export * from "./enums";
-// export * from './hooks'
+export * from "./hooks";
 // export * from './styles'
 // export * from "./types";
-// export * from './utils'
+export * from "./utils";
